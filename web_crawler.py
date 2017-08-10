601a27e13315bca83b41618937ee7c883c019acd
zhaoxuyan <526161421@qq.com>
2017-08-10 23:16:12 +0800

    max_page & max_depth
    add def crawl_web_1(seed, max_page):
    and def crawl_web_2(seed, max_depth):
    to limit the running time of the crawler

--- web_crawler.py
+++ web_crawler.py
@@ -22,7 +22,7 @@ def get_all_links(page):
     return links
 
 
-def get_page(url):
+def get_page(url): #得到一个url的全部字符串
     try:
         import urllib
         return urllib.urlopen(url).read()
@@ -37,14 +37,41 @@ def union(a, b):
     return a
 
 
-seed = get_all_links(get_page('http://www.baidu.com'))
+# seed = get_all_links(get_page('http://www.baidu.com'))
 
-
-def crawl_web(seed):
-    tocrawl = seed
+def crawl_web_1(seed, max_page):
+    tocrawl = [seed]
     crawled = []
     while tocrawl:
         # depth-first-search :意味着我们将首先处理最后一条链接
+        page = tocrawl.pop()
+        if page not in crawled and len(crawled) < max_page:
+            crawled.append(page)
+            union(tocrawl, get_all_links(get_page(page)))
+            # tocrawl = tocrawl + get_all_links(get_page(page))
+    return crawled
+
+
+def crawl_web_2(seed, max_depth):
+    tocrawl = [seed]
+    crawled = []
+    next_depth = []
+    depth = 0
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled and depth <= max_depth:
+            crawled.append(page)
+            union(next_depth, get_all_links(get_page(page)))
+        if not tocrawl: # 直到tocrawl为空, 即这一层已经全部纳入crawled
+            tocrawl, next_depth = next_depth, []
+            depth = depth + 1
+    return crawled
+
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
             crawled.append(page)
@@ -52,7 +79,10 @@ def crawl_web(seed):
             # tocrawl = tocrawl + get_all_links(get_page(page))
     return crawled
 
-print crawl_web(seed)
+print get_all_links(get_page("http://www.baidu.com"))
+print crawl_web_1("http://www.baidu.com", 2)
+print crawl_web_2("http://www.baidu.com", 1) # 由于depth = 1的每一个页面都要crawl next_depth, 所耗时间过长
+# print crawl_web_1("http://www.baidu.com", 2)
 
 
 
